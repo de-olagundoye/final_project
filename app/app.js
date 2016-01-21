4b9e044b9dcab2686da582e6b5db0f12d70abc8a
De Olagundoye <de.olagundoye@gmail.com>
2016-01-20 22:22:28 -0500

    adds index page and server

---
+++ app/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var db;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/sandbox';
+MongoClient.connect(mongoUrl, function(err, database) {
+  if (err) { throw err; }
+  db = database;
+  process.on('exit', db.close);
+});
+
+app.get('/', function (req, res){
+  res.render('landing')
+});
+
+app.get('/about', function (req, res){
+  res.render('about')
+})
+
+app.listen(process.env.PORT || 3000);
